cd6b207f930a94d703b1b6cc1d3fb8cfdf1316a2
Sarah Ankers <mumspride@hotmail.co.uk>
2024-08-03 14:26:23 +0000

    create an array of flags and countries

---
+++ flag.js
@@ -0,0 +1,50 @@
+const questionsArray = [
+    {
+        country: "France",
+        flagImage: "assets/france-flag.jpg", // Ensure you have this image in your assets folder
+        population: "65.2 million",
+        language: "French",
+        capital: "Paris",
+        options: ["Germany", "France", "Austria", "Norway"] // The correct answer should be included
+    },
+    {
+        country: "Germany",
+        flagImage: "assets/germany-flag.jpg",
+        population: "83.2 million",
+        language: "German",
+        capital: "Berlin",
+        options: ["Italy", "Netherlands", "Germany", "Belgium"]
+    },
+    {
+        country: "Japan",
+        flagImage: "assets/japan-flag.jpg",
+        population: "126.3 million",
+        language: "Japanese",
+        capital: "Tokyo",
+        options: ["China", "South Korea", "Japan", "Thailand"]
+    },
+    {
+        country: "Brazil",
+        flagImage: "assets/brazil-flag.jpg",
+        population: "212.6 million",
+        language: "Portuguese",
+        capital: "Brasília",
+        options: ["Argentina", "Brazil", "Colombia", "Chile"]
+    },
+    {
+        country: "Canada",
+        flagImage: "assets/canada-flag.jpg",
+        population: "37.6 million",
+        language: "English and French",
+        capital: "Ottawa",
+        options: ["United States", "United Kingdom", "Australia", "Canada"]
+    },
+    {
+        country: "India",
+        flagImage: "assets/india-flag.jpg",
+        population: "1.366 billion",
+        language: "Hindi and English",
+        capital: "New Delhi",
+        options: ["Pakistan", "Bangladesh", "India", "Nepal"]
+    }
+];
